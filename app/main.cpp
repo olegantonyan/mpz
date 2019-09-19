3d4e5b4b6182d6d60fa9e82d5c1b94cc3e4f4a68
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2019-09-19 12:19:46 +0200

    taglib build with qmake

--- app/main.cpp
+++ app/main.cpp
@@ -2,18 +2,12 @@
 
 #include <QApplication>
 
-#include "test.h"
-
-#include <QDebug>
-
 int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
 
   QCoreApplication::setOrganizationName("Oleg Antonyan");
   QCoreApplication::setOrganizationDomain("github.com/olegantonyan/mpz");
   QCoreApplication::setApplicationName("mpz");
-
-  qDebug() << Test().test();
 
   MainWindow w;
   w.show();
