a8461c99cbabd7b6575f1e71f3b2d048fcd85869
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2020-08-12 08:43:58 +0200

    version bump to 0.0.3

--- build.rpm.sh
+++ build.rpm.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-VERSION=0.0.1
 SRC_DIR=$(cd `dirname $0` && pwd)
+VERSION=$(grep -oP '(?<=").+(?=\\\\\\\")' version.pri)
 TMP_DIR=$(mktemp -d -t mpz-build-$(date +%Y-%m-%d-%H-%M-%S)-XXXXX)
 TMP_SRC_DIR=mpz-$VERSION
 TMP_SRC_FULL_DIR=$TMP_DIR/mpz-$VERSION
 SPECFILE=mpz.spec
 
+echo -e "version:\t$VERSION"
 echo -e "source dir:\t$SRC_DIR"
 echo -e "tmp dir:\t$TMP_DIR"
 echo -e "tmp source dir:\t$TMP_SRC_DIR"
 echo -e "tmp source full dir:\t$TMP_SRC_FULL_DIR"
 
-mkdir -p $TMP_SRC_FULL_DIR/app
-cp -ar app/*          $TMP_SRC_FULL_DIR/app
-mkdir -p $TMP_SRC_FULL_DIR/libs
-cp -ar libs/*         $TMP_SRC_FULL_DIR/libs
+mkdir -p             $TMP_SRC_FULL_DIR/app
+cp -ar app/*         $TMP_SRC_FULL_DIR/app
+mkdir -p             $TMP_SRC_FULL_DIR/libs
+cp -ar libs/*        $TMP_SRC_FULL_DIR/libs
 cp -ar mpz.pro       $TMP_SRC_FULL_DIR
-cp -ar mpz.desktop       $TMP_SRC_FULL_DIR
+cp -ar mpz.desktop   $TMP_SRC_FULL_DIR
 cp -ar license.txt   $TMP_SRC_FULL_DIR
+cp -ar version.pri   $TMP_SRC_FULL_DIR
 ls -latrh $TMP_SRC_FULL_DIR
 
 tree $TMP_SRC_FULL_DIR
--- version.pri
+++ version.pri
@@ -1,1 +1,1 @@
-DEFINES += VERSION=\\\"0.0.2\\\"
+DEFINES += VERSION=\\\"0.0.3\\\"
