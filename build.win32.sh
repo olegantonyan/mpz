28b64b5cc34af8cc2271635c268cb7c2302c3ec2
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2020-08-23 08:34:45 +0200

    build win32 script

--- build.win32.sh
+++ build.win32.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+##
+QTMINGW_DIR=/mnt/storage/dev/qt/qt515-win32-mingw-static
+MINGW_SYSROOT=/usr/i686-w64-mingw32/sys-root/
+MAKE=mingw32-make
+##
+
 SRC_DIR=$(cd `dirname $0` && pwd)
 VERSION=$(grep -oP '(?<=").+(?=\\\\\\\")' $SRC_DIR/version.pri)
 TMP_DIR=$(mktemp -d -t mpz-build-$(date +%Y-%m-%d-%H-%M-%S)-XXXXX)
 TMP_SRC_DIR=mpz-$VERSION
 TMP_SRC_FULL_DIR=$TMP_DIR/$TMP_SRC_DIR
 
-QTMINGW_DIR=/mnt/storage/dev/qt/qt515-win64-mingw-static
-MINGW_SYSROOT=/usr/x86_64-w64-mingw32/sys-root
+
+
 
 
 echo -e "version:\t$VERSION"
@@ -18,18 +24,18 @@ echo -e "tmp source full dir:\t$TMP_SRC_FULL_DIR"
 
 
 $QTMINGW_DIR/bin/qmake CONFIG+=release $SRC_DIR
-mingw64-make -j8
+$MAKE -j8
 
 mkdir -p $TMP_SRC_FULL_DIR
 cp -R app/mpz.exe                                   $TMP_SRC_FULL_DIR
 cp -R $MINGW_SYSROOT/mingw/bin/libstdc++-6.dll      $TMP_SRC_FULL_DIR
-cp -R $MINGW_SYSROOT/mingw/bin/libgcc_s_seh-1.dll   $TMP_SRC_FULL_DIR
+cp -R $MINGW_SYSROOT/mingw/bin/libgcc_s_sjlj-1.dll   $TMP_SRC_FULL_DIR
 cp -R $MINGW_SYSROOT/mingw/bin/libwinpthread-1.dll  $TMP_SRC_FULL_DIR
 tree $TMP_SRC_FULL_DIR
 
 cd $TMP_DIR
-zip -r $TMP_SRC_DIR-win64.zip $TMP_SRC_DIR
+zip -r $TMP_SRC_DIR-win32.zip $TMP_SRC_DIR
 
 echo "done"
-echo $TMP_DIR/$TMP_SRC_DIR-win64.zip
-mv $TMP_DIR/$TMP_SRC_DIR-win64.zip ~/Desktop
+echo $TMP_DIR/$TMP_SRC_DIR-win32.zip
+mv $TMP_DIR/$TMP_SRC_DIR-win32.zip ~/Desktop
