0421ff21a920e29dc786aa05a2d83a8701ba356e
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2020-09-03 13:54:43 +0200

    fix win build scripts

--- build.win64.sh
+++ build.win64.sh
@@ -5,15 +5,15 @@
 # https://download.qt.io/archive/qt/
 # ./configure -nomake examples -nomake tests -nomake tools -confirm-license -xplatform win32-g++ -device-option CROSS_COMPILE=x86_64-w64-mingw32- -opensource -no-compile-examples -prefix /mnt/storage/dev/qt/qt515-win64-mingw-static -skip qt3d -skip qtandroidextras -skip qtcanvas3d -skip qtcharts -skip qtconnectivity -skip qtdatavis3d -skip qtdeclarative -skip qtdoc -skip qtgamepad -skip qtgraphicaleffects -skip qtimageformats -skip qtlocation -skip qtmacextras -skip qtquickcontrols -skip qtquickcontrols2 -skip qtscript -skip qtscxml -skip qtsensors -skip qtserialbus -skip qtserialport -skip qtspeech -skip activeqt -no-opengl -static -no-pch -optimize-size && make -j8 && make install
 
+SRC_DIR=$(cd `dirname $0` && pwd)
+VERSION=$(grep -oP '(?<=").+(?=\\\\\\\")' $SRC_DIR/version.pri)
+
 ##
 QTMINGW_DIR=/mnt/storage/dev/qt/qt515-win64-mingw-static
 MINGW_SYSROOT=/usr/x86_64-w64-mingw32/sys-root
 MAKE=mingw64-make
 ARTIFACT_NAME=mpz-$VERSION-win64-static.exe
 ##
-
-SRC_DIR=$(cd `dirname $0` && pwd)
-VERSION=$(grep -oP '(?<=").+(?=\\\\\\\")' $SRC_DIR/version.pri)
 
 
 echo -e "version:\t$VERSION"
