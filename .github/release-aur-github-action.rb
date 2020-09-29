7e011a4f0f95aba536d948094e05a480e9c82284
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2020-09-29 15:49:16 +0200

    f1x

--- .github/release-aur-github-action.rb
+++ .github/release-aur-github-action.rb
@@ -22,7 +22,7 @@ source=("$pkgname-$pkgver.zip::<%= source %>")
 sha256sums=('<%= sha256sums %>')
 
 build() {
-    cd $pkgname-$pkgver
+    cd $pkgname-master
 
     rm -rf build
     mkdir build
@@ -32,7 +32,7 @@ build() {
 }
 
 package() {
-    cd $pkgname-$pkgver
+    cd $pkgname-master
 
     cd build
     make install INSTALL_ROOT=$pkgdir
