cd877887aaaaa4917ea031bc463e1ac4585bea4e
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2021-11-28 10:44:36 +0200

    aur improve script

--- .github/release-aur-github-action.rb
+++ .github/release-aur-github-action.rb
@@ -123,8 +123,9 @@ puts "***** END OF SRCINFO *****"
   git_ssh = "GIT_SSH_COMMAND='ssh -i #{keyfile} -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'"
 
   puts "cloning aur repo..."
-  puts "#{git_ssh} git clone --branch master #{aur_repo}"
-  `cd #{d} && #{git_ssh} git clone --branch master #{aur_repo}`
+  clone_cmd = "#{git_ssh} git clone --branch master #{aur_repo}"
+  puts clone_cmd
+  `cd #{d} && #{clone_cmd}`
 
   ::File.open("#{d}/mpz/PKGBUILD", 'w') { |f| f.write(pkgbuild) }
   ::File.open("#{d}/mpz/.SRCINFO", 'w') { |f| f.write(srcinfo) }
