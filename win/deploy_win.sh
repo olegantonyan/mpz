b8db95f301b66baddb28cd64159cda56f64fe190
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2022-02-19 20:16:31 +0200

    add win deploy script

--- win/deploy_win.sh
+++ win/deploy_win.sh
@@ -5,3 +5,5 @@ export PATH=$PATH:/e/Qt/Tools/mingw810_64/bin/:/e/Qt/5.15.2/mingw81_64/bin/
 
 windeployqt.exe ./app/mpz.exe --dir deploy/
 cp ./app/mpz.exe deploy/
+
+# gh release upload 1.0.19 ~/Desktop/mpz-1.0.19-win64-dynamic.zip
