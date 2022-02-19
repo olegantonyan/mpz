b5ca17a1e93551b63879c7675259ca152161290c
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2022-02-19 20:04:40 +0200

    add win deploy script

--- win/deploy_win.sh
+++ win/deploy_win.sh
@@ -1,7 +1,7 @@
 # Run inside build dir
-# pack ./deploy/ as distrubution
+# pack ./mpz/ as distrubution
 
 export PATH=$PATH:/e/Qt/Tools/mingw810_64/bin/:/e/Qt/5.15.2/mingw81_64/bin/
 
-windeployqt.exe ./app/mpz.exe --dir deploy/
-cp ./app/mpz.exe deploy/
+windeployqt.exe ./app/mpz.exe --dir mpz/
+cp ./app/mpz.exe mpz/
