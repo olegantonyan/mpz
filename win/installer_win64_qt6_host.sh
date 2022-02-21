55a2e5dff0ebffc2d8c6dc31195aa61591ef2cbd
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2022-02-21 12:05:44 +0200

    windows installer

---
+++ win/installer_win64_qt6_host.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+export PATH=$PATH:/e/Qt/Tools/mingw900_64/bin/:/e/Qt/6.2.3/mingw_64/bin/:/e/Qt/Tools/QtInstallerFramework/4.2/bin
+
+SRC_DIR=$(cd `dirname $0` && cd .. && pwd)
+VERSION=$(grep -oP '(?<=").+(?=\\\\\\\")' $SRC_DIR/version.pri)
+TMP_DIR=$(mktemp -d -t mpz-build-win64-qt6-$(date +%Y-%m-%d-%H-%M-%S)-XXXXX)
+cd $TMP_DIR
+
+cp -R $SRC_DIR/win/installer .
+mkdir installer/packages/mpz/data
+
+ARTIFACT_PATH=installer/packages/mpz/data/
+ARTIFACT_NAME=mpz-$VERSION-win64-qt6-installer.exe
+
+echo -e "version:\t$VERSION"
+echo -e "source dir:\t$SRC_DIR"
+echo -e "build dir:\t$TMP_DIR"
+
+qmake.exe CONFIG+=release $SRC_DIR && mingw32-make.exe -j6
+windeployqt.exe ./app/mpz.exe --dir $ARTIFACT_PATH
+cp ./app/mpz.exe $ARTIFACT_PATH
+
+cd installer
+cp $SRC_DIR/license.txt packages/mpz/meta/
+
+sed -Ei "s/<Version>(.*)<\/Version>/<Version>$VERSION<\/Version>/g" ./config/config.xml
+sed -Ei "s/<Version>(.*)<\/Version>/<Version>$VERSION<\/Version>/g" ./packages/mpz/meta/package.xml
+sed -Ei "s/<ReleaseDate>(.*)<\/ReleaseDate>/<ReleaseDate>$(date +%Y-%m-%d)<\/ReleaseDate>/g" ./packages/mpz/meta/package.xml
+
+cat ./config/config.xml
+cat ./packages/mpz/meta/package.xml
+
+binarycreator.exe -c config/config.xml -p packages $ARTIFACT_NAME
+cp ./$ARTIFACT_NAME $HOME/Desktop
+
+
