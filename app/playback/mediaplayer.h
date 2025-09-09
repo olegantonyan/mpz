46660397c1b97206dcd76ce30cfb59095c7b4664
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2025-09-09 19:19:07 +0200

    add hack for pipewire stream name (pause by stop and rewind) - add define to disable it at compile time

--- app/playback/mediaplayer.h
+++ app/playback/mediaplayer.h
@@ -12,7 +12,7 @@
   #include <QAudioDevice>
   #define QT6_STREAM_HACKS
 #endif
-#if defined(Q_OS_LINUX) && (QT_VERSION >= QT_VERSION_CHECK(6, 0, 0))
+#if defined(Q_OS_LINUX) && (QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)) && !defined(DISABLE_PIPEWIRE_STREAM_NAME_HACK)
   #define PIPEWIRE_STREAM_NAME_HACK
 #endif
 
