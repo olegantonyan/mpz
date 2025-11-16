cf5b66f4ae9b9d68c2a28577ec3b37c3e8be0016
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2025-11-16 19:23:31 +0100

    extract mpd_track factory

--- app/playlist_ui/mpd/playlistmodel.cpp
+++ app/playlist_ui/mpd/playlistmodel.cpp
@@ -1,4 +1,5 @@
 #include "playlistmodel.h"
+#include "playlist/mpd_track.h"
 
 #include <QDebug>
 #include <QtConcurrent>
@@ -47,30 +48,8 @@ namespace PlaylistUi {
 
         struct mpd_song *song;
         while ((song = mpd_recv_song(connection.conn)) != NULL) {
-          const char *uri = mpd_song_get_uri(song);
-          const char *title = mpd_song_get_tag(song, MPD_TAG_TITLE, 0);
-          const char *artist = mpd_song_get_tag(song, MPD_TAG_ARTIST, 0);
-          const char *album = mpd_song_get_tag(song, MPD_TAG_ALBUM, 0);
-          const char *genre = mpd_song_get_tag(song, MPD_TAG_GENRE, 0);
-          const char *tracknum  = mpd_song_get_tag(song, MPD_TAG_TRACK, 0);
-          const char *year   = mpd_song_get_tag(song, MPD_TAG_DATE, 0);
-
-          Track track(
-            QString::fromUtf8(uri),
-            0,
-            artist ? QString::fromUtf8(artist) : QString(),
-            album ? QString::fromUtf8(album) : QString(),
-            title ? QString::fromUtf8(title) : QString(),
-            tracknum ? QString::fromUtf8(tracknum).toInt() : 0,
-            year ? QString::fromUtf8(year).toInt() : 0,
-            mpd_song_get_duration(song) * 1000,
-            0,
-            0,
-            0
-          );
-          track.generateUidByHashing(playlist_name);
-          result << track;
-
+          result << Playlist::MpdTrack::build(song, playlist_name);
+          
           mpd_song_free(song);
         }
 
