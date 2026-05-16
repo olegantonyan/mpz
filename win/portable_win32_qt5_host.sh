72daea6de2afd050d92505869fa778a5cd6aeae4
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2026-05-16 09:08:26 +0200

    add windows build on actions (#176)

--- win/portable_win32_qt5_host.sh
+++ win/portable_win32_qt5_host.sh
@@ -1,38 +1,43 @@
 #!/bin/bash
 
-QT_VERSION=5.15.2
+if [ -z "${QTDIR:-}" ]; then
+    QT_VERSION=5.15.2
 
-QT_INSTALL_PATH=/e/Qt
+    QT_INSTALL_PATH=/e/Qt
 
-QTDIR=$QT_INSTALL_PATH/$QT_VERSION/mingw81_32/
-TOOLCHAIN_PATH=$QT_INSTALL_PATH/Tools/mingw810_32/bin
-CMAKE_PATH=$QT_INSTALL_PATH/Tools/CMake_64/bin
-NINJA_PATH=$QT_INSTALL_PATH/Tools/Ninja
+    QTDIR=$QT_INSTALL_PATH/$QT_VERSION/mingw81_32/
+    TOOLCHAIN_PATH=$QT_INSTALL_PATH/Tools/mingw810_32/bin
+    CMAKE_PATH=$QT_INSTALL_PATH/Tools/CMake_64/bin
+    NINJA_PATH=$QT_INSTALL_PATH/Tools/Ninja
 
-export PATH=$PATH:$TOOLCHAIN_PATH:$QTDIR/bin/:$CMAKE_PATH:$NINJA_PATH
-export QTDIR
+    export PATH=$PATH:$TOOLCHAIN_PATH:$QTDIR/bin/:$CMAKE_PATH:$NINJA_PATH
+    export QTDIR
 
-echo -e "Qt path:\t$QTDIR"
-echo -e "Toolchain:\t$TOOLCHAIN_PATH"
-echo -e "CMake:\t$CMAKE_PATH"
-echo -e "Ninja:\t$NINJA_PATH"
+    echo -e "Qt path:\t$QTDIR"
+    echo -e "Toolchain:\t$TOOLCHAIN_PATH"
+    echo -e "CMake:\t$CMAKE_PATH"
+    echo -e "Ninja:\t$NINJA_PATH"
+fi
 
 SRC_DIR=$(cd `dirname $0` && cd .. && pwd)
 VERSION=$(`dirname $0`/_extract_version.sh)
+OUTPUT_DIR=${MPZ_OUTPUT_DIR:-$HOME/Desktop}
 TMP_DIR=$(mktemp -d -t mpz-build-win32-$(date +%Y-%m-%d-%H-%M-%S)-XXXXX)
 cd $TMP_DIR
 
-ARTIFACT_NAME=mpz-$VERSION-win32-qt5-portable/
+ARTIFACT_NAME=mpz-$VERSION-win32-qt5-portable
 
 echo -e "version:\t$VERSION"
 echo -e "source dir:\t$SRC_DIR"
 echo -e "build dir:\t$TMP_DIR"
+echo -e "output dir:\t$OUTPUT_DIR"
 
 cmake -DCMAKE_BUILD_TYPE=Release -GNinja -DUSE_QT5=ON $SRC_DIR && ninja
 windeployqt.exe ./mpz.exe --dir $ARTIFACT_NAME
 cp ./mpz.exe $ARTIFACT_NAME
-rm -rf $HOME/Desktop/$ARTIFACT_NAME
-cp -R $ARTIFACT_NAME $HOME/Desktop/
+rm -rf "$OUTPUT_DIR/$ARTIFACT_NAME" "$OUTPUT_DIR/$ARTIFACT_NAME.zip"
+cp -R "$ARTIFACT_NAME" "$OUTPUT_DIR/"
+(cd "$OUTPUT_DIR" && 7z a "$ARTIFACT_NAME.zip" "$ARTIFACT_NAME")
 
 echo -e "version:\t$VERSION"
 echo -e "source dir:\t$SRC_DIR"
